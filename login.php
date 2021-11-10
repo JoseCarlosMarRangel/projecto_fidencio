c4f4c20f979726df53f9afeb7ed9a6b06c010d21
JoseCarlosMarRangel <1930082@upv.edu.mx>
2021-11-10 02:34:51 -0600

    ya hay conexion

--- login.php
+++ login.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 require 'conexion.php';
 
@@ -23,30 +22,4 @@ if($query){
 
 }
 
-=======
-<?php
-require 'conexion.php';
-
-$numero = $_POST['NumeroE'];
-$nombre = $_POST['NombreE'];
-
-
-date_default_timezone_set('America/Mexico_City');
-$fecha_actual=date("Y-m-d H:i:s");
-
-$insertar = "INSERT INTO asistencia VALUES('$numero','$nombre','fecha_actual')";
-
-$query = mysqli_query($conectar,$insertar);
-
-if($query){
-
-	echo "<script> alert ('correcto');<script>";
-
-}else{
-
-	echo "<script> alert('incorrecto');<script>";
-
-}
-
->>>>>>> a909d36dd7ff28c219ee569e718aaafa4d369934
 ?>
