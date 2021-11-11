944639a34aaa1c653fdbbdd5a0df8144bf9a21b5
José Carlos Mar Rangel <68357760+JoseCarlosMarRangel@users.noreply.github.com>
2021-11-11 11:38:07 -0600

    pestaña justificante

---
+++ justificante.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="justificas.css">
+</head>
+<body>
+
+<h1>REGISTRO DE ASISTENCIA</h1>
+
+
+	<form action="" method="POST">
+		<textArea></textArea>
+	</form>
+
+	<button id="regresar" onclick="location.href='index.html'">SALIR</button>
+	<button id="confirmar" onclick="location.href='index.html'">CONFIRMAR</button>
+	
+</body>
+</html>
+
+
+
